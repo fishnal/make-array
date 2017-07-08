25e1a84333942e5d75f1f0de7d7ff221503d4ef9
Vishal Patel <patelvishal3199@gmail.com>
2017-07-08 16:12:05 -0400

    Fixed app.js bugs and made it a bit more concise
    - If the input was empty, an attempt to make the array string is still made, which results in the Array header popping up even though no array was formed
    - Made some constants for whether the language is Java, JS, or Ruby (makes it a bit easier to use if you need to access these values again)
    - Fixed a bug with the isStringArr check; would accept "2x" for example through parseFloat, but through a Number cast it results in NaN; so two results are added, one from the parseFloat and the Number cast, and the array is assumed to be a string if either of these two conditions are met: if either result (parse of cast) is NaN or if the desired language is Java and the float result is not the same as the cast result
    - If the array has strings, then each value is iterated and has escape sequences inserted where appropriate
    - If the array has strings, then there's no need to check if the array has ints
    - Local variable "content" visible to the entire scope of the makeArray() function
    - Instead of forming the array in separate if-branches for Java and JavaScript, the two branches are now joined and separate checks are made to ensure proper array declaration for each language
    - Removed the document.getElementById(id) statement and replaced it with jQuery; you can still change CSS properties in jQuery through $(selector).css(attribute, newValue) [see https://api.jquery.com/css/]

--- app.js
+++ app.js
@@ -12,15 +12,19 @@ function getInput() {
   lang = $(".btn:first-child").val();
 
   // Tell user if they have forgotten to select a language
-  if (lang == "")
+  if (lang == "") {
     alert("Please choose a language!");
+    return;
+  }
 
   // Get enter values
   var input = $("#input").val();
 
   // Tell user if they have forgotten to enter values
-  if (input == "")
+  if (input == "") {
     alert("Please enter values!")
+    return;
+  }
 
   // If user input contains Comma Separated Values
   if (input.includes(",")) {
@@ -48,64 +52,83 @@ function getInput() {
 }
 
 function makeArray() {
+  const isJava = lang == "Java";
+  const isJS = lang == "JavaScript";
+  const isRuby = lang == "Ruby";
+
   // Check if the values are numbers
   var isStringArr = false;
-  for (var i = 0; i < values.length; i++)
-    if (isNaN(parseFloat(values[i])))
+  for (var i = 0; i < values.length; i++) {
+    var cast = Number(values[i]);
+    var parse = parseFloat(values[i]);
+    // Because Java is strictly typed, it'd be safe to assume that
+    // the value is a string in Java if
+    // 1. the language desired is Java
+    // 2. the Number cast and parseFloat results are not the same
+    if (isNaN(cast) || isNaN(parse) || isJava && cast != parse) {
       isStringArr = true;
-
-  // Check if values are double or int
-  var isIntArr = true;
-  for (var i = 0; i < values.length; i++)
-    if (values[i].includes("."))
-      isIntArr = false;
-
-  // If language is Java
-  if (lang == "Java") {
-    if (isStringArr) {
-      var content = "String[] arr = {";
-      for (var i = 0; i < values.length; i++) {
-        if (i != values.length - 1)
-          content += '"' + values[i] + '"' + ", ";
-        else
-          content += '"' + values[i] + '"' + "};";
-      }
-      document.getElementById("array").style.display = "";
-      $("#array").append(content)
-    }
-    else {
-      // If array contains ints
-      if (isIntArr) {
-        var content = "int[] arr = {";
-        for (var i = 0; i < values.length; i++) {
-          if (i != values.length - 1)
-            content += values[i] + ", ";
-          else
-            content += values[i]  + "};";
-        }
-
-        document.getElementById("array").style.display = "";
-        $("#array").append(content)
-      }
-      // If array contains doubles
-      else {
-        var content = "double[] arr = {";
-        for (var i = 0; i < values.length; i++) {
-          if (i != values.length - 1)
-            content += values[i] + ", ";
-          else
-            content += values[i]  + "};";
-        }
-
-        document.getElementById("array").style.display = "";
-        $("#array").append(content)
-      }
+      break;  // Don't need to iterate through anymore
     }
   }
-  else if (lang == "JavaScript") {
 
+  if (isStringArr) {
+    var escapes = ['\'', '\"', '\\'];
+    // Go through all the values and account for escape characters
+    for (var i = 0; i < values.length; i++) {
+      var str = values[i];
+
+      for (var j = 0; j < escapes.length; j++)
+        str = str.replace(escapes[j], "\\" + escapes[j]);
+
+      values[i] = str;
+    }
+  }
+
+  // Check if values are double or int
+  var isIntArr = false;
+  if (!isStringArr) {
+    // Don't bother checking for ints if we already have strings
+    isIntArr = true;
+    for (var i = 0; i < values.length; i++)
+      // Check if a point or exponential sign is present
+      if (values[i].includes(".") || values[i].includes("e")) {
+        isIntArr = false;
+        break; // Don't need to iterate through anymore
+      }
+  }
+  
+  var content = "";
+
+  // If language is Java or JavaScript
+  if (isJava || isJS) {
+    if (isJava) {
+      if (isStringArr)
+        content = "String[]";
+      else if (isIntArr)
+        content = "int[]";
+      else
+        content = "double[]";
+      content += " arr = { ";
+    } else
+      content = 'var arr = [ ';
+
+    for (var i = 0; i < values.length; i++) {
+      if (isStringArr)
+        content += '"' + values[i] + '"';
+      else
+        content += values[i];
+
+      if (i != values.length - 1)
+        content += ", ";
+    }
+
+    content += " " + (isJava ? "}" : "]") + ";";
   }
   else if (lang == "Ruby") {
     
   }
+
+  var element = $('#array');
+  element.append(content + "<br/>");
+  element.css("display", "");
 }
